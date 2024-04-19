548f9e7c8484a91754687c5f1a4951c59a1a36ac
tordbm <tordbm@icloud.com>
2024-04-19 16:48:15 +0200

    add simple table

---
+++ src/components/ContentLoader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="d-flex justify-content-center mt-3">
+    <div class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  <slot></slot>
+</template>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,17 +1,35 @@
 <template>
-  <button class="btn btn-light" @click="fetchData">Show your mom</button>
+  <button class="btn btn-light" @click="fetchData">Show Stats</button>
+  <ContentLoader v-if="loading" />
   <div v-if="response && !loading">
-    {{ response.data }}
+    <table class="table table-hover mt-2 center">
+      <thead>
+        <tr>
+          <th>{{ response.data.head.vars[0] + ' id' }}</th>
+          <th>{{ response.data.head.vars[1] }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in response.data.results.bindings">
+          <td>{{ parseURI(item.staff.value) }}</td>
+          <td>{{ item.lastname.value }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import axios from 'axios'
+import ContentLoader from './ContentLoader.vue'
 
 const queryUrl = 'http://localhost:8080/sparql'
 
 export default defineComponent({
+  components: {
+    ContentLoader,
+  },
   data() {
     return {
       response: null as any,
@@ -33,18 +51,27 @@ export default defineComponent({
             PREFIX obda: <https://w3id.org/obda/vocabulary#>
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-            SELECT ?person ?gender
+            SELECT ?staff ?lastname
             WHERE {
-            ?person rdf:type ex:Customer .
-            ?person ex:gender ?gender .
+            ?staff rdf:type ex:Staff .
+            ?staff ex:last_name ?lastname .
             }
+            LIMIT 50
+            
             `,
         },
       })
-      console.log(response)
       this.response = response
       this.loading = false
+    },
+    parseURI(uri: string): string {
+      return '' + uri.match(/[^/]*$/)
     },
   },
 })
 </script>
+<style scoped lang="scss">
+th::first-letter {
+  text-transform: capitalize;
+}
+</style>
